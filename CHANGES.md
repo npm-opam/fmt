d34fe78e65562a62806166f39c79f5e2db26678f
Yunxing Dai <yunxing@fb.com>
2016-10-20 10:28:07 -0700

    init commit

---
+++ CHANGES.md
@@ -0,0 +1,26 @@
+
+v0.8.0 2016-05-23 La Forclaz (VS)
+---------------------------------
+
+* Build depend on topkg.
+* Relicense from BSD3 to ISC.
+* Tweak `Fmt.Dump.option` to indent like in sources.
+* Add `Fmt.Dump.signal` formatter for `Sys` signal numbers.
+* Add `Fmt[.Dump].result`, formatter for `result` values.
+* Add `Fmt.{words,paragraphs}` formatters on US-ASCII strings.
+* Add `Fmt.exn[_backtrace]`. Thanks to Edwin Török for suggesting.
+* Add `Fmt.quote`.
+* Rename `Fmt.text_range` to `Fmt.text_loc` and simplify output
+  when range is a position.
+
+v0.7.1 2015-12-03 Cambridge (UK)
+--------------------------------
+
+* Add optional cmdliner support. See the `Fmt_cli` module provided
+  by the package `fmt.cli`.
+
+
+v0.7.0 2015-09-17 Cambridge (UK)
+--------------------------------
+
+First Release.
---
+++ test/test.ml
@@ -0,0 +1,47 @@
+(*---------------------------------------------------------------------------
+   Copyright (c) 2015 Daniel C. Bünzli. All rights reserved.
+   Distributed under the ISC license, see terms at the end of the file.
+   fmt v0.8.0
+  ---------------------------------------------------------------------------*)
+
+let test_utf_8 () =
+  let ppf = Format.formatter_of_buffer (Buffer.create 23) in
+  assert (Fmt.utf_8 ppf = true);
+  Fmt.set_utf_8 ppf false;
+  assert (Fmt.utf_8 ppf = false);
+  Fmt.set_utf_8 ppf true;
+  assert (Fmt.utf_8 ppf = true);
+  ()
+
+let test_style_renderer () =
+  let ppf = Format.formatter_of_buffer (Buffer.create 23) in
+  assert (Fmt.style_renderer ppf = `None);
+  Fmt.set_style_renderer ppf `Ansi_tty;
+  assert (Fmt.style_renderer ppf = `Ansi_tty);
+  Fmt.set_style_renderer ppf `None;
+  assert (Fmt.style_renderer ppf = `None);
+  ()
+
+let tests () =
+  test_utf_8 ();
+  test_style_renderer ();
+  Printf.printf "Done.\n";
+  ()
+
+let () = tests ()
+
+(*---------------------------------------------------------------------------
+   Copyright (c) 2015 Daniel C. Bünzli
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+  ---------------------------------------------------------------------------*)
